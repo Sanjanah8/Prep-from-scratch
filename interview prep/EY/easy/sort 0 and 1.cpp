dba6aef192d53327028d24155f2a507f0d9d7134
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2025-01-13 15:27:55 +0530

    Create sort 0 and 1.cpp

---
+++ interview prep/EY/easy/sort 0 and 1.cpp
@@ -0,0 +1,16 @@
+void sortZeroesAndOne(int input[], int size)
+{
+    int left=0,right=size-1;
+    while(left < right){
+        if(input[left]==0){
+            left++;
+        }else if(input[right]==1){
+            right--;
+        }
+        else{
+swap(input[left],input[right]);
+left++;
+right--;
+        }
+    }
+}
